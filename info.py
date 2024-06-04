8450966c81dd2d565a8d83ce5816f721d2ea8f6e
IMMORTAL-DEVELOPER <91306144+IMMORTAL-DEVELOPER@users.noreply.github.com>
2024-06-04 15:35:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -139,7 +139,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://mytestvj-998c9929dc7a.herokuapp.com/")
+URL = environ.get("URL", "https://filmaxstrem-684b1da4bd4b.herokuapp.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
